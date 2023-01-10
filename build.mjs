04dbd5ce54459e2763f381c891ebefca66c8d941
takaya1992 <tky.c10ver@gmail.com>
2023-01-11 08:20:34 +0900

    build runner

---
+++ build.mjs
@@ -0,0 +1,33 @@
+import process from 'node:process';
+import { build } from 'esbuild';
+import glob from 'glob';
+import packageJson from './package.json' assert { type: 'json' };
+
+glob('src/**/*.ts', (err, matches) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  const shared = {
+    bundle: true,
+    entryPoints: matches,
+    outdir: './dist',
+    external: Object.keys(packageJson.dependencies ?? {}),
+    logLevel: 'info',
+    sourcemap: false,
+    target: ['node18'],
+  };
+
+  build({
+    ...shared,
+    format: 'esm',
+    entryNames: '[dir]/[name].esm',
+  });
+
+  build({
+    ...shared,
+    format: 'cjs',
+    entryNames: '[dir]/[name].cjs',
+  });
+});
